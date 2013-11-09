e64782edf8e93de4dd6b24ff62d64590d4cb80b5
Aidan <aidan.mclaughlin@students.olin.edu>
2013-11-09 13:02:32 -0500

    working spoon

--- SoupforTim/src/main/java/com/example/soupfortim/MainActivity.java
+++ SoupforTim/src/main/java/com/example/soupfortim/MainActivity.java
@@ -1,74 +1,92 @@
 package com.example.soupfortim;
 
-import android.graphics.drawable.AnimationDrawable;
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
-import android.graphics.Canvas;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
 
-    static float x = 0;
-    static float y =0;
+    static int x = 0;
+    static int y =0;
+    int wh;
+    int ww;
 
     DrawingPanel drawingPanel;
     Canvas canvas;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawingPanel = new DrawingPanel(this);
-        canvas = new Canvas();
+
+        wh = getWindowManager().getDefaultDisplay().getHeight();
+        ww = getWindowManager().getDefaultDisplay().getWidth();
+
+        Log.d(String.valueOf(wh), "heightuwant");
+        Log.d(String.valueOf(ww), "widthuneed");
+
+        final RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 
         final ImageView spoon = (ImageView) this.findViewById(R.id.spoon);
         spoon.setImageResource(R.drawable.spoon_icon);
         spoon.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == motionEvent.ACTION_DOWN){
-                    x = motionEvent.getX();
-                    y = motionEvent.getY();
-                    Log.d(String.valueOf(x), "we touched the screen");
-                    drawingPanel.postInvalidate();
-                    drawingPanel.onDraw(canvas);
-                    return true;
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        x = Math.round(motionEvent.getRawX());
+
+                        y = Math.round(motionEvent.getRawY());
+                        Log.d(String.valueOf(x),"were moving");
+                        Log.d(String.valueOf(y),"were moving");
+
+
+                        lp.setMargins(x-50 , y-175,0,0);
+                        spoon.setLayoutParams(lp);
                 }
-                if (motionEvent.getAction() == motionEvent.ACTION_MOVE){
-                    x = motionEvent.getX();
-                    y = motionEvent.getY();
-                    Log.d(String.valueOf(x),"were moving");
-                    drawingPanel.postInvalidate();
-                    drawingPanel.onDraw(canvas);
-                    return true;
-                }
-                drawingPanel.postInvalidate();
+
                 return true;
+
+
+//                if (motionEvent.getRawX() > ww-5) {
+//                     x = ww;
+//                }
+//                if (motionEvent.getRawY() > wh-5) {
+//                    y = wh;
+//                }
+//                if (motionEvent.getAction() == motionEvent.ACTION_DOWN){
+////                    drawingPanel.postInvalidate();
+////                    drawingPanel.onDraw(canvas);
+//                    return true;
+//                }
+//                if (motionEvent.getAction() == motionEvent.ACTION_MOVE){
+//                    x = Math.round(motionEvent.getRawX());
+//
+//                    y = Math.round(motionEvent.getRawX());
+//                    Log.d(String.valueOf(x),"were moving");
+//                    Log.d(String.valueOf(y),"were moving");
+//
+//
+//                    lp.setMargins(x-25 , y-75,0,0);
+//                    spoon.setLayoutParams(lp);
+//
+////                    drawingPanel.postInvalidate();
+////                    drawingPanel.onDraw(canvas);
+//                    return true;
+//                }
+////                drawingPanel.animate();
+//                return true;
             }
+
         });
-
-/*        // Load the ImageView that will host the animation and
-        // set its background to our AnimationDrawable XML resource.
-        ImageView img = (ImageView) this.findViewById(R.id.tim);
-        img.setBackgroundResource(R.drawable.spin_animation);
-
-        // Get the background, which has been compiled to an AnimationDrawable object.
-        AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
-
-        // Start the animation (looped playback by default).
-        frameAnimation.start();*/
     }
-
-
-/*    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(android.R.menu.main, menu);
-        return true;
-    }
-    */
 }
