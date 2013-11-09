b64fb0672f173f26532eac6444a4e852baed0680
Berko <alison.berkowitz@students.olin.edu>
2013-11-08 22:56:19 -0500

    Tims face is on my tablet

--- SoupforTim/src/main/java/com/example/soupfortim/MainActivity.java
+++ SoupforTim/src/main/java/com/example/soupfortim/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.soupfortim;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -10,14 +12,28 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView img = (ImageView) this.findViewById(R.id.tim);
+        img.setImageResource(R.drawable.tima);
+
+/*        // Load the ImageView that will host the animation and
+        // set its background to our AnimationDrawable XML resource.
+        ImageView img = (ImageView) this.findViewById(R.id.tim);
+        img.setBackgroundResource(R.drawable.spin_animation);
+
+        // Get the background, which has been compiled to an AnimationDrawable object.
+        AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+
+        // Start the animation (looped playback by default).
+        frameAnimation.start();*/
     }
 
 
-    @Override
+/*    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(android.R.menu.main, menu);
         return true;
     }
-    
+    */
 }
